87e6473f048ac9b1579b5e6ece15dbfe9f24701e
Vanessa Dickey <vcdickey@gmail.com>
2023-12-06 01:20:58 +0000

    pre-websocket sandboxing

--- fastapi-frontend/src/websocket/createWebSocket.js
+++ fastapi-frontend/src/websocket/createWebSocket.js
@@ -2,7 +2,10 @@
 
 const createWebSocket = () => {
     const ws = new WebSocket("ws://localhost:8000/ws");
+    // const ws = new WebSocket("wss://8000-vanessadick-cloakchatfa-9f77pe9ilo5.ws-us106.gitpod.io/ws");
     
 }
 
 export default createWebSocket;
+
+// function (!isOpen(socket)) return
