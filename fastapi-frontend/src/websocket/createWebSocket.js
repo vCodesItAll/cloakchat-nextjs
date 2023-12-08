e5a2d419de9425d735a622824dd8851583983949
Vanessa Dickey <vcdickey@gmail.com>
2023-12-08 21:58:37 +0000

    committing before trying justin's websocket

--- fastapi-frontend/src/websocket/createWebSocket.js
+++ fastapi-frontend/src/websocket/createWebSocket.js
@@ -1,6 +1,7 @@
 
 
 const createWebSocket = () => {
+    // const ws = new WebSocket("wss://8000-vanessadick-cloakchatfa-9f77pe9ilo5.ws-us106.gitpod.io/");
     const ws = new WebSocket("wss://8000-vanessadick-cloakchatfa-9f77pe9ilo5.ws-us106.gitpod.io/");
     ws.addEventListener("open", (event) => {
         ws.send("hi");
