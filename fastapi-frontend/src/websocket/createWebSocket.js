98bd039b7058c97bfb2a9a6f64a18fa2e5dfe38f
Vanessa Dickey <vcdickey@gmail.com>
2024-01-15 14:26:42 +0000

    replaced web socket url

--- fastapi-frontend/src/websocket/createWebSocket.js
+++ fastapi-frontend/src/websocket/createWebSocket.js
@@ -1,8 +1,8 @@
-
+import { WEB_SOC_URL } from "@/services/auth.constants";
 
 const createWebSocket = () => {
     // const ws = new WebSocket("wss://8000-vanessadick-cloakchatfa-9f77pe9ilo5.ws-us106.gitpod.io/");
-    const ws = new WebSocket("wss://8000-vanessadick-cloakchatfa-9f77pe9ilo5.ws-us106.gitpod.io/");
+    const ws = new WebSocket(WEB_SOC_URL);
     ws.addEventListener("open", (event) => {
         ws.send("hi");
     });
